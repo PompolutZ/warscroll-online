360bffb2dbe6702602b22ddcdcaac05274150e93
Oleh Lutsenko <oleh.lutsenko@tobii.com>
2017-09-05 15:15:01 +0200

    add profile image

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+// const logo = require('./logo.svg');
+// const profileHalo = require('./imgs/profilehalo.png');
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        {/* <div className="App-header">
+          <img src={profileHalo} className="App-logo" alt="logo" />
+          <h2>Welcome to React!!!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div className="profile-container">
+          <img src={profileHalo} className="profile-halo" alt="logo" />
+        </div> */}
+        <div className="parent">
+          <div className="child">
+          </div>
+        </div>
       </div>
     );
   }
