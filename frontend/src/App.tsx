102a5a4ba0a004c15e4cd4eb28ee81af77820546
Oleh Lutsenko <olo@tobii.com>
2017-10-11 16:41:10 +0200

    fix minor ts-lint stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-import { Grid, Col, Row } from 'react-bootstrap';
+import { Grid, Col, Row, DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
 
 import { ProfileHalo } from './components/profileHalo/index';
 import { WeaponProfiles } from './components/weaponProfile/index';
@@ -13,19 +13,53 @@ import { Link, Switch, Route } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <h1><Link to='/'>Home</Link></h1>
+      <h1><Link to="/">Home</Link></h1>
     </header>  
   );
-}
+};
 
-const WarscrollLink = (props: any) => {
-  return (
-    <h4>
-      <Link to={`/warscroll/${props.name}`}>
-        {props.name}
-      </Link>
-    </h4>
-  );
+class WarscrollLink extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      carrying: [],
+      index: -1
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch(`/ws/${this.props.name}`);
+    const ws = await data.json();
+    this.setState({
+      carrying: ws.carries
+    });
+  }
+
+  renderDropDown = (items: string[], i: number) => {
+    const renderMenuItems = (item: string, it: number) => {
+      return <MenuItem eventKey={i} key={it}>{items.join('/')}</MenuItem>;
+    };
+
+    return (
+      <DropdownButton bsStyle="primary" title="Choose weapon" id={`dropdown-basic-${i}`}>
+        {items.map(renderMenuItems)}
+      </DropdownButton>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>
+          <Link to={`/warscroll/${this.props.name}`}>
+            {this.props.name}
+          </Link>
+        </h4>
+        <ButtonToolbar>{this.renderDropDown(this.state.carrying, this.state.index)}</ButtonToolbar>
+      </div>
+    );
+  }
 }
 
 class Home extends React.Component<any, any> {
@@ -34,7 +68,7 @@ class Home extends React.Component<any, any> {
 
     this.state = {
       warscrolls: []
-    }
+    };
   }
 
   async componentDidMount() {
@@ -47,8 +81,8 @@ class Home extends React.Component<any, any> {
 
   render() {
     const links: any = 
-      this.state.warscrolls.map((el: string, i:number) => {
-        return <WarscrollLink name={el} key={i} /> 
+      this.state.warscrolls.map((el: string, i: number) => {
+        return <WarscrollLink name={el} key={i} />; 
       });
 
     return (
@@ -61,12 +95,12 @@ const Main = () => {
   return (
     <main>
       <Switch>
-        <Route exact path='/' component={Home}></Route>
-        <Route path='/warscroll/:name' component={Warscroll}></Route>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/warscroll/:name" component={Warscroll} />
       </Switch>  
     </main>  
   );
-}
+};
 
 class Warscroll extends React.Component<any, any> {
   constructor(props: any) {
